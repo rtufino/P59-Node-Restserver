12dea89c16d95d45a79559adba4bc207764ce336
Rodrigo Tufino <rtufino@ups.edu.ec>
2022-01-24 08:30:17 -0500

    Agregado PUT y Get

--- server/models/usuario.js
+++ server/models/usuario.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
+
 let Schema = mongoose.Schema;
 
 let rolesValidos = {
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -4,13 +4,40 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 // Importar el Schema de uaurio
 const Usuario = require('../models/usuario');
+// Iportar undescore
+const _ = require('underscore');
 // Crear el objeto app
 const app = express();
 
 // Configurar los endpoint para usuario
 /*Consultar datos*/
 app.get('/usuario', (req, res) => {
-    res.json('get Usuario');
+
+    let desde = req.query.desde || 0;
+    desde = Number(desde)
+
+    let limite = req.query.limite || 5;
+    limite = Number(limite)
+
+    Usuario.find({})
+        .skip(desde)
+        .limit(limite)
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            Usuario.count({}, (err, conteo) => {
+                res.json({
+                    ok: true,
+                    registros: conteo,
+                    usuarios
+                });
+            });
+        });
 });
 /*crear nuevos registros */
 app.post('/usuario', (req, res) => {
@@ -43,19 +70,26 @@ app.post('/usuario', (req, res) => {
 });
 /*actualizar registros */
 app.put('/usuario/:id', (req, res) => {
-    let body = req.body;
+    let id = req.params.id;
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            mensaje: "El nombre es necesario"
-        });
-    } else {
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+    // delete body.password;
+    // delete body.google;
+
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true, context: 'query' }, (err, usuarioBD) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
         res.json({
-            persona: body
+            ok: true,
+            usuario: usuarioBD
         });
-    }
-
-
+    });
 });
 
 /*Eliminar registros (cambiar a inactivo) */
